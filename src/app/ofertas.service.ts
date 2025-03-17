e87ae166300b43f50767a4a25460426b506f0797
Marco-Merini <marcoleonemerini@gmail.com>
2025-03-17 17:57:18 -0300

    proximo passo, ajustar router

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,87 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Oferta } from './shared/oferta.model';
+import { Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+
+@Injectable()
 
 export class OfertasService {
 
-    public ofertas: Oferta[] = [
-        {
-            id: 1,
-            categoria: "restaurante",
-            titulo: "Super Burger",
-            descricao_oferta: "Rodízio de Mini-hambúrger com opção de entrada.",
-            anunciante: "Original Burger",
-            valor: 29.90,
-            destaque: true,
-            imagens: [
-                {url: "/assets/ofertas/1/img1.jpg"},
-                {url: "/assets/ofertas/1/img2.jpg"},
-                {url: "/assets/ofertas/1/img3.jpg"},
-                {url: "/assets/ofertas/1/img4.jpg"}
-            ]
-        },
-        {
-            id: 2,
-            categoria: "restaurante",
-            titulo: "Cozinha Mexicana",
-            descricao_oferta: "Almoço ou Jantar com Rodízio Mexicano delicioso.",
-            anunciante: "Mexicana",
-            valor: 32.90,
-            destaque: true,
-            imagens: [
-                {url: "/assets/ofertas/2/img1.jpg"},
-                {url: "/assets/ofertas/2/img2.jpg"},
-                {url: "/assets/ofertas/2/img3.jpg"},
-                {url: "/assets/ofertas/2/img4.jpg"}
-            ]
-        
-        },
-        {
-            id: 4,
-            categoria: "diversao",
-            titulo: "Estância das águas",
-            descricao_oferta: "Diversão garantida com piscinas, trilhas e muito mais.",
-            anunciante: "Estância das águas",
-            valor: 31.90,
-            destaque: true,
-            imagens: [
-                {url: "/assets/ofertas/3/img1.jpg"},
-                {url: "/assets/ofertas/3/img2.jpg"},
-                {url: "/assets/ofertas/3/img3.jpg"},
-                {url: "/assets/ofertas/3/img4.jpg"},
-                {url: "/assets/ofertas/3/img5.jpg"},
-                {url: "/assets/ofertas/3/img6.jpg"}
-            ]
-        }
-    ]
+    constructor(private http: HttpClient) {}
 
-    public getOfertas(): Oferta[] {
-        return this.ofertas
-    }
-
-    public getOfertas2(): Promise<Oferta[]> {
-        return new Promise<Oferta[]>((resolve, reject) => {
-            // Algum tipo de processamento, que ao finalizar, chama a função resolve ou reject
-            //console.log('será que passou por aqui?')
-            var deu_certo = true
-            if(deu_certo){
-                setTimeout(() => resolve( this.ofertas ), 3000)
-            } else
-                reject({codigo_erro: 404, mensagem_erro: 'Servidor não encontrado Mestre'})
-        })
-        .then(( ofertas: Oferta[] ) => {
-            //fazer alguma tratativa
-            console.log('primeiro then')
-            return ofertas
-        })
-        .then(( ofertas: Oferta[] ) => {
-            //fazer uma outra tratativa
-            console.log('segundo then')
-            return new Promise<Oferta[]>((resolve2, reject2) => {
-                setTimeout(() => { resolve2( ofertas ) }, 3000) 
-            })
-        })
-        .then(( ofertas: Oferta[] ) => {
-            console.log('terceiro then executado após 3 segundos porque estava aguardando uma promise ser resolvida')
-            return ofertas
-        })
+    public getOfertas(): Promise<Oferta[]> {
+        //efetuar uma requisição http
+        //retornar um promisse Oferta[]
+        return lastValueFrom(
+            this.http.get<Oferta[]>('http://localhost:3000/ofertas'))
     }
 }
