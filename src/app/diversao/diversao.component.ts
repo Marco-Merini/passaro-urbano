6131a89356b915efc3e64fc0ff598f4812628d4a
Marco-Merini <marcoleonemerini@gmail.com>
2025-03-18 17:27:18 -0300

    navbar de como usar e onde fica sobre a oferta, utilizando da forma correta os styles no angular.json

--- src/app/diversao/diversao.component.ts
+++ src/app/diversao/diversao.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Oferta } from './../shared/oferta.model';
+import { OfertasService } from '../ofertas.service'
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-diversao',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './diversao.component.html',
-  styleUrl: './diversao.component.scss'
+  styleUrl: './diversao.component.scss',
+  providers: [OfertasService]
 })
-export class DiversaoComponent {
+export class DiversaoComponent implements OnInit {
+  
+  public ofertas: Oferta[] = []
+
+  constructor(private ofertaService: OfertasService) { }
+
+  ngOnInit() {
+    this.ofertaService.getOfertasPorCategoria('diversao')
+      .then(( ofertas: Oferta[] ) => {
+        this.ofertas = ofertas
+      })
+  }
 
 }
--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Oferta } from './shared/oferta.model';
 import { Injectable } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
+import { URL_API } from './api';
 
 @Injectable()
 
@@ -13,6 +14,34 @@ export class OfertasService {
         //efetuar uma requisição http
         //retornar um promisse Oferta[]
         return lastValueFrom(
-            this.http.get<Oferta[]>('http://localhost:3000/ofertas'))
+            this.http.get<Oferta[]>(`${URL_API}/ofertas?destaque=true`))
+    }
+
+    public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
+        return lastValueFrom(
+            this.http.get<Oferta[]>(`${URL_API}/ofertas?categoria=${categoria}`))
+    }
+
+    public getOfertaPorId(id: number): Promise<Oferta[]> {
+        return lastValueFrom(
+            this.http.get<Oferta[]>(`${URL_API}/ofertas?id=${id}`))
+    }
+
+    public getComoUsarOfertaPorId(id: number): Promise<string> {
+        return lastValueFrom(
+            this.http.get<any[]>(`${URL_API}/como-usar?id=${id}`))
+            .then(response => {
+                const item = response.find(oferta => oferta.id === id);
+                return item ? item.descricao : 'Descrição não encontrada';
+            });
+    }
+
+    public getOndeFicaOfertaPorId(id: number): Promise<string> {
+        return lastValueFrom(
+            this.http.get<any[]>(`${URL_API}/onde-fica?id=${id}`))
+            .then(response => {
+                const item = response.find(oferta => oferta.id === id);
+                return item ? item.descricao : 'Descrição não encontrada';
+            });
     }
 }
